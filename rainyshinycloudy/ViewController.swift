df827548c7699688fb861fedbfbd8739cc4987ba
Nikos Kon <nikoskontothanasis@users.noreply.github.com>
2017-08-10 18:18:50 +0300

    Initial Commit

---
+++ rainyshinycloudy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rainyshinycloudy
+//
+//  Created by Nikos Kontothanasis on 10/08/2017.
+//  Copyright © 2017 Nikos Kontothanasis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
